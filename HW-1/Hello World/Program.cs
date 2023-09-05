3fe3fd84d48e7eaf2578c46c94ce7967af03b95f
KingOfMongeese <nwohletz@gmail.com>
2023-09-04 21:09:26 -0600

    HW1: Hello World Final

---
+++ HW-1/Hello World/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hello_World
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Hello World!");
+
+        }
+
+    }
+
+}
+
