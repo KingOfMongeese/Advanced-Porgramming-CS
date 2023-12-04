d87923064bfdfe98598788c60fc9d90e0750fcc8
KingOfMongeese <nwohletz@gmail.com>
2023-12-03 19:04:17 -0700

    add student scores

---
+++ HW-4-Student-Scores/StudentScores/StudentScores/frmAddNewStudent.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentScores
+{
+    public partial class frmAddNewStudent : Form
+    {
+        public frmAddNewStudent()
+        {
+            InitializeComponent();
+        }
+
+        List<int> scoresList = new List<int>();
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (IsValidScore())
+            {
+                int score = Convert.ToInt32(txtScore.Text);
+                scoresList.Add(score);
+                DisplayScores();
+                txtScore.Focus();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            scoresList.Clear();
+            lblScores.Text = "";
+            txtScore.Focus();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (IsValidName())
+            {
+                string studentScores = txtName.Text;
+                foreach (int score in scoresList)
+                {
+                    studentScores += $"|{score}";
+                }
+                Tag = studentScores;
+                DialogResult = DialogResult.OK;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void DisplayScores()
+        {
+            StringBuilder sb = new StringBuilder(); 
+            foreach (int score in scoresList)
+            {
+                sb.Append($"{score} ");
+            }
+            lblScores.Text = sb.ToString();
+        }
+
+        private bool IsValidScore()
+        {
+            bool success = true;
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(IsPresent(txtScore.Text, "Score"));
+            sb.Append(IsInt32(txtScore.Text, "Score"));
+            sb.Append(IsWithinRange(txtScore.Text, "Score", 0, 100));
+            string errorMsg = sb.ToString();
+
+            if (!String.IsNullOrEmpty(errorMsg))
+            {
+                success = false;
+                MessageBox.Show(errorMsg, "Entry Error");
+            }
+
+            return success;
+        }
+
+        private bool IsValidName()
+        {
+            bool success = true; 
+            string errorMsg = IsPresent(txtName.Text, "Name");
+            if (!String.IsNullOrEmpty(errorMsg))
+            {
+                success = false;
+                MessageBox.Show(errorMsg, "Entry Error");
+            }
+            return success;
+        }
+
+        private string IsPresent(string value, string name)
+        {
+            string errorMsg = "";
+            if (String.IsNullOrEmpty(value)) {
+                errorMsg = $"{name} is a required field.\n";
+            }
+            return errorMsg;
+        }
+
+        private string IsInt32(string value, string name)
+        {
+            string errorMsg = "";
+            if (!Int32.TryParse(value, out _))
+            {
+                errorMsg = $"{name} must be a valid integer.\n";
+            }
+            return errorMsg;
+        }
+
+        private string IsWithinRange(string value, string name, decimal min, decimal max)
+        {
+            string errorMsg = "";
+            if (Decimal.TryParse(value, out decimal number))
+            {
+                if (number < min || number > max)
+                {
+                    errorMsg = $"{name} must be between {min} and {max}.\n";
+                }
+            }
+            return errorMsg;
+        }
+
+
+    }
+}
---
+++ HW-4-Student-Scores/StudentScores/StudentScores/frmStudentScores.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentScores
+{
+    public partial class frmStudentScores : Form
+    {
+        public frmStudentScores()
+        {
+            InitializeComponent();
+        }
+
+        public List<string> studentScores = new List<string>();
+
+        private void frmStudentScores_Load(object sender, EventArgs e)
+        {
+            studentScores.Add("Bill Chipman|92|95|85");
+            studentScores.Add("Jane Doe|100|98|89");
+            studentScores.Add("John Smith|85|79|90");
+            LoadStudentListBox();
+        }
+
+        private void LoadStudentListBox(int selectedIndex = 0)
+        {
+            lstStudents.Items.Clear();
+
+            foreach(string s in studentScores)
+            {
+                lstStudents.Items.Add(s);
+            }
+
+            if (lstStudents.Items.Count > 0)
+            {
+                lstStudents.SelectedIndex = selectedIndex;
+            }
+            else
+            {
+                ClearLabels();
+            }
+
+            lstStudents.Focus();
+
+        }
+
+        private void ClearLabels()
+        {
+            lblScoreCount.Text = "";
+            lblAverage.Text = "";
+            lblScoreTotal.Text = "";
+        }
+
+        private void lstStudents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstStudents.SelectedIndex != -1)
+            {
+                string student = studentScores[lstStudents.SelectedIndex].ToString();
+                string[] scores = student.Split('|');
+
+                int total = 0;
+                for (int i = 1; i < scores.Length; i++)
+                {
+                    total += Convert.ToInt32(scores[i]);
+                }
+                int count = scores.Length - 1;
+
+                int average = 0;
+
+                if (count > 0)
+                {
+                    average = total / count;
+                }
+
+                lblScoreTotal.Text = total.ToString();
+                lblScoreCount.Text = count.ToString();
+                lblAverage.Text = average.ToString();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (studentScores.Count > 0)
+            {
+                studentScores.RemoveAt(lstStudents.SelectedIndex);
+                LoadStudentListBox();
+            }
+        }
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            Form addForm = new frmAddNewStudent();
+            DialogResult result = addForm.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                studentScores.Add(addForm.Tag?.ToString());
+                int lastIndex = studentScores.Count - 1;
+                LoadStudentListBox(lastIndex);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (studentScores.Count > 0 )
+            {
+                int selectedIndex = lstStudents.SelectedIndex;
+                string student = studentScores[selectedIndex].ToString();
+
+                Form updateForm = new frmUpdateStudent();
+                updateForm.Tag = student;
+
+                DialogResult result = updateForm.ShowDialog();
+                if (result == DialogResult.OK) {
+                    studentScores.RemoveAt(selectedIndex);
+                    studentScores.Insert(selectedIndex, updateForm.Tag?.ToString());
+
+                    LoadStudentListBox(selectedIndex);
+                }
+            }
+        }
+    }
+}
